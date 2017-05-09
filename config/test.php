4c0fe0e48bd649717a8231c51791199243900925
Murat KURT <paralaks@gmail.com>
2017-05-09 03:20:56 -0400

    Codeception packages installed/updated

---
+++ config/test.php
@@ -0,0 +1,50 @@
+<?php
+$params = require(__DIR__ . '/params.php');
+$dbParams = require(__DIR__ . '/db.php');
+
+/**
+ * Application configuration shared by all test types
+ */
+return [
+    'id' => 'basic-tests',
+    'basePath' => dirname(__DIR__),
+    'language' => 'en-US',
+    'components' => [
+        'db' => $dbParams,
+        'mailer' => [
+            'useFileTransport' => true,
+        ],
+        'assetManager' => [
+            'basePath' => __DIR__ . '/../web/assets',
+        ],
+        'urlManager' => [
+            'showScriptName' => true,
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+        ],
+        'request' => [
+            'cookieValidationKey' => 'test',
+            'enableCsrfValidation' => false,
+            // but if you absolutely need it set cookie domain to localhost
+            /*
+            'csrfCookie' => [
+                'domain' => 'localhost',
+            ],
+            */
+        ],
+        'i18n' => [
+        'translations' => [
+          'main' => [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@app/translations', // if advanced application, set @frontend/messages
+            'sourceLanguage' => 'en',
+            'fileMap' => [
+              'main' => 'main.php'
+            ]
+          ]
+        ]
+      ],
+    ],
+    'params' => $params,
+];
