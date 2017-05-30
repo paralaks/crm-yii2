4e7f8d4b02a08bbf6373ad13a24bcc1fa19f074b
Murat KURT <paralaks@gmail.com>
2017-05-29 22:09:38 -0400

    RBAC checking added to REST controllers

--- controllers/restv1/BaseRestController.php
+++ controllers/restv1/BaseRestController.php
@@ -14,4 +14,14 @@ class BaseRestController extends ActiveController
 
     return $behaviors;
   }
+
+  public function checkAccess($action, $model = null, $params = [])
+  {
+    if ($model)
+      if (Yii::$app->user->can('/'.strtolower($model->modelClassName()).'/*', [$model]) &&
+        (Yii::$app->user->can('Update '.$model->modelClassName(), [$model]) || Yii::$app->user->can(ucfirst($action).' '.$model->modelClassName(), [$model])))
+        return;
+      else
+        throw new \yii\web\UnauthorizedHttpException('You are not authorized to perform that operation.');
+  }
 }
